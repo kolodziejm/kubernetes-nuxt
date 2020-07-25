2e616e59e7d3ce08c715cfe81d71b2b5b3f0919b
Marcin Kołodziej <marcinkolodziej15@gmail.com>
2020-07-25 13:22:02 +0200

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  async asyncData({ $axios }) {
+    const response = await $axios.get('/api');
+
+    return {
+      message: response.data,
+    }
+  },
+}
+</script>
